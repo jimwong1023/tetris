91eee05be958d6fed1edbb6ab099eda2f3bd2887
Jimmy <jimwong1023@gmail.com>
2014-08-05 11:54:29 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tetris::Application.config.secret_key_base = '7c356166927b83ca0bd6a8d7659e25a369b46f18adb57da34f8ea5ad30412eef586ce21ac2782468619d4d4af599223e1c6f556ff20149800230a2714d6ee951'
