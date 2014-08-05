b16717020618553922b2d550041c87665c890b92
Jimmy <jimwong1023@gmail.com>
2014-08-05 16:52:06 -0700

    css defaults

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Tetris::Application.routes.draw do
+
+  root to: 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
